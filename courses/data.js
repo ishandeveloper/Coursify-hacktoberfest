236fd271476896619e601ef1052dd26d8ad066b3
Ishan Sharma <ishandeveloper@outlook.com>
2020-10-01 20:09:18 +0530

    Resolved Conflicts

--- courses/data.js
+++ courses/data.js
@@ -18,7 +18,6 @@ let data = [
     image: "intro-react.png",
     tags: ["react", "javascript", "web"],
   },
-
   {
     name: "Flutter Complete Course",
     instructor: "Pawan Kumar",
@@ -28,7 +27,6 @@ let data = [
     image: "",
     tags: ["flutter", "app"],
   },
-
   {
     name: "Intro To Linux",
     instructor: "Ishan Sharma",
@@ -42,17 +40,47 @@ let data = [
     name: "Neural Networks and Deep Learning",
     instructor: "Andrew Ng",
     url: "https://www.coursera.org/learn/neural-networks-deep-learning",
-    description: "If you want to break into cutting-edge AI, this course will help you do so. Deep learning engineers are highly sought after, and mastering deep learning will give you numerous new career opportunities. Deep learning is also a new 'superpower' that will let you build AI systems that just weren't possible a few years ago.",
+    description:
+      "If you want to break into cutting-edge AI, this course will help you do so. Deep learning engineers are highly sought after, and mastering deep learning will give you numerous new career opportunities. Deep learning is also a new 'superpower' that will let you build AI systems that just weren't possible a few years ago.",
     image: "",
     tags: ["machine learning", "ai", "deep learning", "neural networks"],
   },
   {
-    name:'Svelte Tutorial for Beginners',
-    instructor: 'NetNinja',
-    url:'https://www.youtube.com/playlist?list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO',
-    description: "in this Svelte tutorial for beginners I'll explain what Svelte is, what we'll be making & what you'll already need to know before starting",
-    image: 'svelte-beginner.jpg',
-    tags:['Technology', 'Framework',"javascript", "web","svelte"]
+    name: "React Native Tutorial for Beginners",
+    instructor: "The Net Ninja",
+    url:
+      "https://www.youtube.com/watch?v=ur6I5m2nTvk&list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ",
+    description:
+      "Hey gang, and welcome to your first React Native tutorial for beginners. In this series we'll go from novice to ninja and create a React Native app from scratch. irst though, we'll get set up and talk about what React Native actually is. ",
+    image: "",
+    tags: ["react-native", "javascript"],
   },
-  
+  {
+    name: "Fullstack React GraphQL TypeScript Tutorial",
+    instructor: "Ben Awad",
+    url: "https://www.youtube.com/watch?v=I6ypD7qv3Z8",
+    description:
+      "A fullstack project course from scratch using React, Next.js, TypeScript, GraphQL, NodeJs, ORM, PostgreSQL.",
+    image: "",
+    tags: ["react", "graphql", "typescript", "javascript"],
+  },
+  {
+    name: "Data Structures (in Python)",
+    instructor: "LucidProgramming",
+    url: "https://goo.gl/TdJpMi",
+    description:
+      "get familiar with common data structures that are frequently tested on in coding interviews.",
+    image: "",
+    tags: ["python", "data structures"],
+  },
+  {
+    name: "Svelte Tutorial for Beginners",
+    instructor: "NetNinja",
+    url:
+      "https://www.youtube.com/playlist?list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO",
+    description:
+      "in this Svelte tutorial for beginners I'll explain what Svelte is, what we'll be making & what you'll already need to know before starting",
+    image: "svelte-beginner.jpg",
+    tags: ["Technology", "Framework", "javascript", "web", "svelte"],
+  },
 ];
