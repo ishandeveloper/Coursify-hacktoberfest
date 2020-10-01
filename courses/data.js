3d8f9a4fe6c55f70cb6df21e2151f51b20419939
dannyflatiron <dannyflatiron@gmail.com>
2020-10-01 14:32:46 -0400

    added data

--- courses/data.js
+++ courses/data.js
@@ -122,4 +122,12 @@ let data = [
     image: '',
     tags:['Flutter', 'Firebase','App development']
 }
+{
+  name:'Clustering Data in Google Maps and React',
+  instructor: 'Leigh Halliday',
+  url:'https://www.youtube.com/watch?v=-NI5e_GTIko&t=1734s',
+  description:'Loading remote data from an API,clustering it, and zooming in to expand a cluster.',
+  image: '',
+  tags:['React', 'Javascript','Google Maps API']
+}
 ];
