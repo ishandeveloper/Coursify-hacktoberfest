bb275e09d5acaf8b7e8561d4bb05fbc3bd977592
Ishan Sharma <ishandeveloper@outlook.com>
2020-10-01 16:05:49 +0530

    first commit

---
+++ courses/data.js
@@ -0,0 +1,41 @@
+let data = [
+  {
+    name: "Node JS Tutorial",
+    instructor: "NetNinja",
+    url:
+      "https://www.youtube.com/playlist?list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp",
+    description:
+      "A step by step walk-through what Node JS is, and how it all works. We'll also be creating a Node JS application from scratch using express, and hook it up to MongoDB - a noSQL database perfect for using with Node.",
+    image: "",
+    tags: ["nodejs", "javascript", "web"],
+  },
+  {
+    name: "Intro To React",
+    instructor: "Ishan Sharma",
+    url: "https://blog.ishandeveloper.com/react-intro",
+    description:
+      "A comprehensive beginner friendly guide to what react is and how you can benefit from it.",
+    image: "intro-react.png",
+    tags: ["react", "javascript", "web"],
+  },
+
+  {
+    name: "Flutter Complete Course",
+    instructor: "Pawan Kumar",
+    url: "https://www.youtube.com/watch?v=Ib2FlirtcmE",
+    description:
+      "Get Started with Flutter and learn how to build natively compiled applications for mobile (Android & iOS), web, and desktop from a single codebase.",
+    image: "",
+    tags: ["flutter", "app"],
+  },
+
+  {
+    name: "Intro To Linux",
+    instructor: "Ishan Sharma",
+    url: "https://blog.ishandeveloper.com/linux-intro",
+    description:
+      "One of the most important tools for a programmer is the Operating System, it runs your whole device and it’s important to choose the one that suits you the best!",
+    image: "intro-linux.png",
+    tags: ["linux", "bash"],
+  },
+];
