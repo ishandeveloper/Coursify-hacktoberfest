b6390f59f63f728b096e76c8a75560825a4d54ca
Bhargav Gohil <bhargavgohil25@gmail.com>
2020-10-02 11:35:06 +0530

    Added Back-To-Top Button on the Page

--- main.js
+++ main.js
@@ -52,3 +52,32 @@ coursesFilteredByTag.forEach((course) => {
         </div>
   `);
 });
+
+/*
+  Code For Back-Top-Top button
+*/
+const scrollToTopButton = document.getElementById('js-top');
+
+const scrollFunc = () => {
+  let y = window.scrollY;
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 10);
+  }
+};
+
+scrollToTopButton.onclick = function(e) {
+  e.preventDefault();
+  scrollToTop();
+}
