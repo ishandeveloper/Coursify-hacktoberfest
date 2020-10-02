2368e982095e5ef2dd2286d01fee7af4c94188e7
Darshan R <darshandarsh27blr@gmail.com>
2020-10-02 16:45:30 +0530

    Added hero images

--- courses/data.js
+++ courses/data.js
@@ -6,7 +6,7 @@ let data = [
       "https://www.youtube.com/playlist?list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp",
     description:
       "A step by step walk-through what Node JS is, and how it all works. We'll also be creating a Node JS application from scratch using express, and hook it up to MongoDB - a noSQL database perfect for using with Node.",
-    image: "",
+    image: "node-js.png",
     tags: ["nodejs", "javascript", "web"],
   },
   {
@@ -32,7 +32,7 @@ let data = [
     url: "https://www.youtube.com/watch?v=Ib2FlirtcmE",
     description:
       "Get Started with Flutter and learn how to build natively compiled applications for mobile (Android & iOS), web, and desktop from a single codebase.",
-    image: "",
+    image: "flutter-course.png",
     tags: ["flutter", "app"],
   },
   {
@@ -60,7 +60,7 @@ let data = [
     url: "https://www.coursera.org/learn/neural-networks-deep-learning",
     description:
       "If you want to break into cutting-edge AI, this course will help you do so. Deep learning engineers are highly sought after, and mastering deep learning will give you numerous new career opportunities. Deep learning is also a new 'superpower' that will let you build AI systems that just weren't possible a few years ago.",
-    image: "",
+    image: "neural-network-course.png",
     tags: ["machine learning", "ai", "deep learning", "neural networks"],
   },
   {
@@ -70,7 +70,7 @@ let data = [
       "https://www.youtube.com/watch?v=ur6I5m2nTvk&list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ",
     description:
       "Hey gang, and welcome to your first React Native tutorial for beginners. In this series we'll go from novice to ninja and create a React Native app from scratch. irst though, we'll get set up and talk about what React Native actually is. ",
-    image: "",
+    image: "react-native.png",
     tags: ["react-native", "javascript"],
   },
   {
