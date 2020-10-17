8b9623a98c7112ef8bffdc73a1ca4ec021cd06e6
Sego <leketi.s@gmail.com>
2020-10-17 14:21:11 +0200

    "ADDED : [Building Serverless Web Applications with React & AWS Amplify]"

--- courses/data.js
+++ courses/data.js
@@ -636,5 +636,13 @@ let data = [
   description: "A great resource for the community to learn Strapi for free!, master the fundamentals of Strapi. This course walks you through all the steps and best practices to build a successful project with Strapi.",
   image: "strapi.png",
   tags: ["strapi", "javascript", "headless cms", "Self-hosted", "content-rich"],
+},
+{
+  name: "Building Serverless Web Applications with React & AWS Amplify",
+  instructor: "Nader Dabit",
+  url: "https://egghead.io/courses/building-serverless-web-applications-with-react-aws-amplify",
+  description: "This course walks you through setup and implementation to get your cloud-based react native application up and running.",
+  image: "",
+  tags: ["react", "javascript", "react native", "aws", "amplify"],
 }
 ];
