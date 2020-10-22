3f6fbd08d8ecf22e30411cb07935eff6c319d735
HG9040626 <68577284+HG9040626@users.noreply.github.com>
2020-10-22 23:00:19 +0530

    Sum.java
    Java program to take a number from command line argument and print .

---
+++ Sum.java
@@ -0,0 +1,13 @@
+class sum
+{
+      public static void main(String ar[])
+      {
+            int x,y,s;
+ 
+            x=Integer.parseInt(ar[0]);
+            y=Integer.parseInt(ar[1]);
+ 
+            s=x+y;
+            System.out.println("sum of " + x + " and " + y +" is " +s);
+      }
+}
