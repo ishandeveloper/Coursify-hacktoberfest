da0b7603b5327ed5dc271718503a75b391706d5c
fransiscusrolandamalau <fransiscusrmalau@gmail.com>
2020-11-25 01:51:31 +0700

    ADDED : VueJs 3 Full Course

--- courses/data.js
+++ courses/data.js
@@ -628,5 +628,13 @@ let data = [
   description:'Baisc concepts related to cloud services including containers; VMs; public, private, hybrib cloud; microservices; DevOps; etc',
   image: 'Cloud.png',
   tags:['cloud', 'containerization', 'virtualization', 'cloud services']
+},
+{
+  name:'VueJs 3 Full Course',
+  instructor: 'Sarthak Shrivastava',
+  url:'https://youtu.be/e-E0UB-YDRk',
+  description:'Vue js tutorial full course for beginners to advanced. Building 10 projects in 10 hours Using Vue 3 composition API - setup, teleport, reactivity, and ref  Vue 3 Tutorial will start from very basics and teach you how to use TensorFlow and firebase with your vuejs in this full course of 10 hours by building 10 different apps',
+  image: '',
+  tags:['Tensorflow JS', 'Vue 3', 'Vuex v4', 'Vue Router v4']
 }
 ];
